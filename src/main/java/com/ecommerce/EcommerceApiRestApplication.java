73817a978bf8292a81fd23cc39b36966a9fee9ca
Pablo <pabloprietolopez94@outlook.es>
2026-03-15 13:37:09 +0100

    Initial commit

---
+++ src/main/java/com/ecommerce/EcommerceApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApiRestApplication.class, args);
+    }
+
+}
