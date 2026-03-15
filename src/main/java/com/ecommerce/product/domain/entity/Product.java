b5af856664b9282c97919d15d627050bf3e20c0d
Pablo <pabloprietolopez94@outlook.es>
2026-03-15 21:05:21 +0100

    feat: add builder

--- src/main/java/com/ecommerce/product/domain/entity/Product.java
+++ src/main/java/com/ecommerce/product/domain/entity/Product.java
@@ -6,12 +6,9 @@ import lombok.Data;
 @Builder
 @Data
 public class Product {
-
     private Long id;
     private String name;
     private String description;
     private Double price;
     private String image;
-
-
 }
