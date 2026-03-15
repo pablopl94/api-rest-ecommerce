facb8a6ffbaf2ad330de5cb25c11edf53ee93a9f
Pablo <pabloprietolopez94@outlook.es>
2026-03-15 21:06:55 +0100

    refactor: add api dtos response/request and refactor interface and controller

---
+++ src/main/java/com/ecommerce/product/infrastructure/api/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.ecommerce.product.infrastructure.api.mapper;
+
+import com.ecommerce.product.application.command.create.ProductCreateRequest;
+import com.ecommerce.product.domain.entity.Product;
+import com.ecommerce.product.infrastructure.api.dto.ProductRequestDto;
+import com.ecommerce.product.infrastructure.api.dto.ProductResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface ProductMapper {
+
+    ProductCreateRequest mapRequestToCreate(ProductRequestDto productRequestDto);
+
+    ProductResponseDto mapProductToResponse(Product product);
+}
