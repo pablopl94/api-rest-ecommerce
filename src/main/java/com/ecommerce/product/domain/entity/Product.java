b5809f068d3d19ce259129c625cf6ed963cb63d2
Pablo <pabloprietolopez94@outlook.es>
2026-03-15 14:32:22 +0100

    feat: add Product entity in domain

---
+++ src/main/java/com/ecommerce/product/domain/entity/Product.java
@@ -0,0 +1,17 @@
+package com.ecommerce.product.domain.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String image;
+
+
+}
