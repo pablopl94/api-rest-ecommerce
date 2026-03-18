61984c9fedb96a31bc8327741ccc213d55d2efd3
Pablo <pabloprietolopez94@outlook.es>
2026-03-18 22:50:29 +0100

    feat: feat: add MultipartFile type to image

--- src/main/java/com/ecommerce/product/infrastructure/api/dto/ProductRequestDto.java
+++ src/main/java/com/ecommerce/product/infrastructure/api/dto/ProductRequestDto.java
@@ -3,18 +3,16 @@ package com.ecommerce.product.infrastructure.api.dto;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.web.multipart.MultipartFile;
 
 public record ProductRequestDto(
 
         @NotBlank(message = "Name must not be empty")
         String name,
-
         @Length(min = 10, max = 250, message = "Description must be between 10 and 250 characters")
         String description,
-
         @DecimalMin(value = "0.99", message = "Price must be greater than or equal to 0.99€", inclusive = false)
         Double price,
-
-        String image
+        MultipartFile image
 ) {
 }
