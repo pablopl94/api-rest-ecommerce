cb50e725d999f5c13a5ee55368bd5f3e9d64f6c1
Pablo <pabloprietolopez94@outlook.es>
2026-03-19 07:40:01 +0100

    chore: add logging and a thread to test async method

--- src/main/java/com/ecommerce/product/application/command/delete/ProductDeleteHandler.java
+++ src/main/java/com/ecommerce/product/application/command/delete/ProductDeleteHandler.java
@@ -3,17 +3,28 @@ package com.ecommerce.product.application.command.delete;
 import com.ecommerce.common.mediator.RequestHandler;
 import com.ecommerce.product.domain.port.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class ProductDeleteHandler implements RequestHandler<ProductDeleteRequest, Void> {
 
     private final ProductRepository productRepository;
+    private final Logger log = LoggerFactory.getLogger(ProductDeleteHandler.class);
 
     @Override
     public Void handle(ProductDeleteRequest request) {
+        log.info("Eliminando producto con id {}", request.getId());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         productRepository.deleteById(request.getId());
+        log.info("Producto correctamente eliminado con id {}", request.getId());
         return null;
     }
 
